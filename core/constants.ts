12bead87262d976242673615569fbe2019b06038
abdul <up2dul@gmail.com>
2024-07-14 01:41:26 +0700

    feat(core): create constants file for colors and size
    Creates a new constants file that defines the colors and sizes used throughout the application. This makes it easier to maintain and update the visual styles of the application.

---
+++ core/constants.ts
@@ -0,0 +1,13 @@
+export const colors = {
+  dark: '#232323',
+  light: '#f8f5f2',
+  primary: '#eaddcf',
+  paragraph: '#594a4e',
+};
+
+export const sizes = {
+  sm: 12,
+  md: 16,
+  lg: 24,
+  xl: 32,
+};
